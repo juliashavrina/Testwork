cf3297d82cabb095fbd9ba3c1fe9e4afe0ec2a63
julia shavrina <tifanys@yandex.ru>
2022-08-10 10:29:34 +0300

    Part2

--- Program.cs
+++ Program.cs
@@ -5,6 +5,27 @@
   for (int i =0; i< animals.Length; i++)
   Console.Write(animals [i] +" "); 
   Console.WriteLine(); 
+  int count = 0;
+  string[] animalsnew = new string [count];
+  string [] animalstemp;
+  for (int k =0; k< animals.Length; k++)
+  {
+  char[] arr;
+  arr = animals [k].ToCharArray();
+    {
+    if (arr.Length <= 3)
+    {
+    count++;
+    string Str = new string(arr);
+    animalstemp = new string [count];
+    for (int l = 0; l < animalstemp.Length -1; l++)
+      animalstemp[l] = animalsnew[l];
+    animalstemp[count -1] = Str;
+    animalsnew = animalstemp;
+    }
+    }
+  }
   
+   
 }
 CreateNewString (args); 
