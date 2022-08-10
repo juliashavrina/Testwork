0ac087644db8981561e916ab10da34b0a16ee682
julia shavrina <tifanys@yandex.ru>
2022-08-10 10:25:06 +0300

    Partone

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿void CreateNewString(string[] args)
+{
+  
+  string[] animals = { "cat", "dog", "cow", "goat","elefante", "pig"};
+  for (int i =0; i< animals.Length; i++)
+  Console.Write(animals [i] +" "); 
+  Console.WriteLine(); 
+  
+   
+}
+CreateNewString (args); 
